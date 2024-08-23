ba591b72c1427566c996c67a8ca5e8d01942c8ed
Alvian Syah <139302290+alviansyahburhani@users.noreply.github.com>
2024-08-23 03:15:52 +0000

    chapter 5

--- app/ui/dashboard/sidenav.js
+++ app/ui/dashboard/sidenav.js
@@ -7,7 +7,7 @@ export default function SideNav() {
   return (
     <div className="flex flex-col h-full px-3 py-4 md:px-2">
       <Link
-        className="flex items-end justify-start h-20 p-4 mb-2 bg-blue-600 rounded-md md:h-40"
+        className="flex items-end justify-start h-20 p-4 mb-2 bg-gradient-to-bl from-pink-100 via-purple-700 to-blue-200 rounded-md md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
@@ -18,7 +18,7 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden w-full h-auto rounded-md grow bg-gray-50 md:block"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gradient-to-bl from-pink-100 via-purple-700 to-blue-200 p-3 text-sm font-medium hover:bg-sky-100 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
